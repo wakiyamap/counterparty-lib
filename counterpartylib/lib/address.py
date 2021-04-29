92b7afbe589408b7e2b8dcf63039bd8852dbeaf3
Jin Eguchi <wakiyamap@gmail.com>
2021-04-30 07:06:24 +0900

    pack segwit version 0 only

--- counterpartylib/lib/address.py
+++ counterpartylib/lib/address.py
@@ -15,7 +15,7 @@ def pack(address):
             bech32 = bitcoin.bech32.CBech32Data(address)
             witver = (0x80 + bech32.witver).to_bytes(1, byteorder='big') # mark the first byte for segwit
             witprog = bech32.to_bytes()
-            if not (0 <= bech32.witver <= 16):
+            if bech32.witver != 0:
                 raise Exception('impossible witness version')
             if len(witprog) == 20:
                 return b''.join([witver, witprog])
